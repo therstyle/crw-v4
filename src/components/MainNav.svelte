af88f9259bba0cb1c3de21028d7ecc1820751dad
Chris Roberts <chris.roberts@rainfocus.com>
2026-01-27 19:04:16 -0500

    Added assets + structure

--- src/components/MainNav.svelte
+++ src/components/MainNav.svelte
@@ -2,6 +2,20 @@
 
 </script>
 
-<div>
-  Welcome
+<div class="crw-main-nav__wrapper">
+  <div class="crw-main-nav__top">
+
+  </div>
+
+  <nav class="crw-main-nav__links">
+
+  </nav>
+
+  <div class="crw-main-nav__bottom">
+
+  </div>
 </div>
+
+<style>
+
+</style>
