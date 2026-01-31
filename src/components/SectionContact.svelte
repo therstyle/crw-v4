40850f41868b02bc101c3b9b21122129083972d7
Chris Roberts <chris.roberts@rainfocus.com>
2026-01-31 14:03:16 -0500

    Load data

--- src/components/SectionContact.svelte
+++ src/components/SectionContact.svelte
@@ -5,7 +5,18 @@
 </script>
 
 <SectionWrapper {id} {title}>
-  <div class="crw-contact"></div>
+  <div class="crw-contact">
+    <ul>
+      {#each items as item, index (index)}
+        <li id={`portfolio-item-${index}`}>
+          <a href={item?.url} target="_blank">
+            <img src={item?.logo} alt={item?.title} />
+            <span>{item?.title}</span>
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </div>
 </SectionWrapper>
 
 <style lang="scss">
--- src/components/SectionPortfolio.svelte
+++ src/components/SectionPortfolio.svelte
@@ -5,7 +5,23 @@
 </script>
 
 <SectionWrapper {id} {title}>
-  <div class="crw-portfolio"></div>
+  <div class="crw-portfolio">
+    {#each items as item, index (index)}
+      <article id={`portfolio-item-${index}}`} class="crw-portfolio__item">
+        <a href={item?.url} target="_blank">
+          <img src={item?.image} alt={item?.title} />
+        </a>
+
+        <div class="crw-portfolio__item-info">
+          <h3>{item?.title}</h3>
+          <p>{@html item?.description}</p>
+          <div class="crw-portfolio__item-details">
+            <a href={item?.url} target="_blank">View Project</a>
+          </div>
+        </div>
+      </article>
+    {/each}
+  </div>
 </SectionWrapper>
 
 <style lang="scss">
