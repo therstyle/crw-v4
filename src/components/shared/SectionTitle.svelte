ac6b11059d9fc546e089fe391a09f7c3d674ef2c
Chris Roberts <chris.roberts@rainfocus.com>
2026-02-02 18:39:52 -0500

    Updated title styling

--- src/components/shared/SectionTitle.svelte
+++ src/components/shared/SectionTitle.svelte
@@ -6,17 +6,21 @@
   let isVisible = $state(false)
 </script>
 
-<header>
+<header class="crw-section-title">
   <h2>{title}</h2>
 </header>
 
 <style lang="scss">
   @use '../../styles/vars';
 
-  h2 {
-    font-size: 88px;
-    font-weight: 800;
-    line-height: 1;
-    text-transform: uppercase;
+  .crw-section-title {
+    padding: 15px 20px 20px var(--space-4);
+
+    h2 {
+      font-size: 88px;
+      font-weight: 800;
+      line-height: 1;
+      text-transform: uppercase;
+    }
   }
 </style>
