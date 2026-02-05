6b10f2d25e6945504caaaef60afe2f5aae41ec82
Chris Roberts <chris.roberts@rainfocus.com>
2026-02-05 13:13:51 -0500

    Updated portfolio style

--- src/components/SectionPortfolio.svelte
+++ src/components/SectionPortfolio.svelte
@@ -6,21 +6,39 @@
 
 <SectionContainer {id} {title}>
   <div class="crw-portfolio">
-    {#each items as item, index (index)}
-      <article id={`portfolio-item-${index}}`} class="crw-portfolio__item">
-        <a href={item?.url} target="_blank">
-          <img src={item?.image} alt={item?.title} loading="lazy" />
-        </a>
+    <div class="crw-portfolio__container">
+      <div class="crw-portfolio__container-left">
+        <button
+          aria-label="Prev"
+          class="crw-portfolio__button crw-portfolio--button-prev"
+        ></button>
+      </div>
 
-        <div class="crw-portfolio__item-info">
-          <h3>{item?.title}</h3>
-          <p>{@html item?.description}</p>
-          <div class="crw-portfolio__item-details">
-            <a href={item?.url} target="_blank">View Project</a>
-          </div>
-        </div>
-      </article>
-    {/each}
+      <div class="crw-portfolio__container-main">
+        {#each items as item, index (index)}
+          <article id={`portfolio-item-${index}`} class="crw-portfolio__item">
+            <a href={item?.url} target="_blank">
+              <img src={item?.image} alt={item?.title} loading="lazy" />
+            </a>
+
+            <div class="crw-portfolio__item-info">
+              <h3>{item?.title}</h3>
+              <p>{@html item?.description}</p>
+              <div class="crw-portfolio__item-details">
+                <a href={item?.url} target="_blank">View Project</a>
+              </div>
+            </div>
+          </article>
+        {/each}
+      </div>
+
+      <div class="crw-portfolio__container-right">
+        <button
+          aria-label="Next"
+          class="crw-portfolio__button crw-portfolio--button-next"
+        ></button>
+      </div>
+    </div>
   </div>
 </SectionContainer>
 
@@ -28,5 +46,25 @@
   @use '../styles/vars.scss';
 
   .crw-portfolio {
+    &__container {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: var(--space-1);
+    }
+
+    &__container-main {
+      flex: 1;
+      display: flex;
+      gap: var(--space-1);
+    }
+
+    &__button {
+      display: flex;
+      width: 48px;
+      height: 48px;
+      border: none;
+      border-radius: 50%;
+    }
   }
 </style>
