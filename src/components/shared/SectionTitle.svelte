dc8af2b2a91a7de326420b15942da729e6a30da4
Chris Roberts <chris.roberts@rainfocus.com>
2026-02-20 18:33:19 -0500

    Added a transition effect for titles

--- src/components/shared/SectionTitle.svelte
+++ src/components/shared/SectionTitle.svelte
@@ -21,16 +21,55 @@
   @use '../../styles/mixins';
 
   .crw-section-title {
-    padding: 15px 20px 20px var(--space-4);
-    padding-block-start: 0;
+    display: inline-grid;
+    grid-template-areas: 'main';
+    margin-right: auto;
+    padding-inline-start: var(--crw-section-title-padding-inline-start);
     @include mixins.slide-up-on-visible;
-
-    @include mixins.max(sm) {
-      padding-inline-start: var(--space-2);
-    }
 
     &--margin-bottom {
       margin-block-end: var(--space-8);
+    }
+
+    --crw-section-title-padding-inline-start: var(--space-5);
+    --crw-section-title-background-offset: var(--space-2);
+
+    @include mixins.max(sm) {
+      --crw-section-title-padding-inline-start: var(--space-3);
+      --crw-section-title-background-offset: var(--space-1);
+    }
+
+    &::before {
+      content: '';
+      grid-area: main;
+      width: calc(100% + (var(--space-4) * 2) + var(--main-nav-width));
+      background: hsla(0, 0%, 100%, 0.1);
+      transition: var(--global-transition);
+      transition-delay: 0.3s;
+      translate: calc(
+          -100% -
+            (
+              var(--main-nav-width) +
+                var(--crw-section-title-padding-inline-start) + var(--space-1)
+            )
+        )
+        0;
+      opacity: 0;
+    }
+
+    &[data-is-visible='true'] {
+      &::before {
+        opacity: 1;
+        translate: calc(
+            (var(--crw-section-title-padding-inline-start) - var(--space-1)) *
+              -1
+          )
+          0;
+        width: calc(
+          100% + var(--crw-section-title-padding-inline-start) +
+            var(--crw-section-title-background-offset)
+        );
+      }
     }
 
     h2 {
@@ -38,6 +77,8 @@
       font-weight: 800;
       line-height: 1;
       text-transform: uppercase;
+      grid-area: main;
+      z-index: 2;
 
       @include mixins.max(sm) {
         font-size: 36px;
