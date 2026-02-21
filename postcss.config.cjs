837d00c3f46b158925bb9f4b90deb4eb0ca336a4
Chris Roberts <chris.roberts@rainfocus.com>
2026-02-21 11:51:43 -0500

    Added postcss

---
+++ postcss.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    require('postcss-image-inliner', { maxFileSize: 10240 }), // Images smaller than 10kb will be inlined
+    require('postcss-preset-env')({
+      stage: 2, // Set the desired stage (e.g., 2 or 3)
+      features: {
+        'logical-properties-and-values': false, // Enable logical properties
+      },
+    }),
+    require('postcss-pxtorem'),
+    // require('cssnano')({
+    //   preset: ['default', {
+    //     discardComments: false,
+    //     normalizeWhitespace: false
+    //   }]
+    // }),
+  ],
+}
